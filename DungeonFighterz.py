792fa37a6736908572bbbd94aa693a82df587e25
DankDoge <dogeisdaawesomememe@gmail.com>
2025-09-12 23:31:06 -0700

    added magical affinity, revamped move return strings, need to work more on armor, and double loss system. Also dodge hitting.

--- DungeonFighterz.py
+++ DungeonFighterz.py
@@ -1,7 +1,7 @@
-import random as r
-from time import sleep as s
-import sys
-import winsound
+import random as r # rng component
+from time import sleep as s # stops program from running for a set time
+import sys # gimmick stuff idrk what the library does specifically
+import winsound # sound, not yet implemented
 
 enemyNameList = []
 num=1
@@ -23,55 +23,56 @@ def fighter class
 hp = hitpoints (how much health the fighter has)
 oghp = permanent hitpoints (how much health the fighter had before the fight)
 at = attack (how much damage the fighter does)
-dg = dodge (chance to dodge attack) [dodge chance = dg / 100]
+dg = dodge (chance to dodge attack) [dodge chance = dg / 100 * 100%]
+acc = accuracy (chance to miss attack) [miss chance = (1 - acc / 100) * 100%]
+ma = magic affinity (how effective heal is) [heal * ma/100]
+arm = armor (extra hp that won't be healed)
 id is used for fighter class's number which is used for lists
 
 IDEAS:
-acc = accuracy (for ranged fighters, similar to how dodge works but opposite, possible balancing tool)
 crt = critical hit (would be a 1.25x, 1.5x, or 2x multiplier to dmg, some classes would specialize in this -- like barbarian!)
-mn = mana (for more mystical fightertypes like wizard or elf. Would have this to deliever unique spells and tricks. Might also turn into an energy system which could help with balancing)
+mn = mana (for more mystical fightertypes like wizard or elf. Would have this to deliever unique spells and tricks. 
+Might also turn into an energy system which could help with balancing)
 """
 class fighter:
     fighterCount = 1
-    def __init__(self, type, hp, at, dg):
+    def __init__(self, type, hp, arm, at, dg, acc, ma):
         self.type = type
         self.hp = round(hp)
         self.oghp = self.hp
+        self.arm = arm
+        self.ogarm = self.arm
         self.at = at
         self.dg = dg
+        self.acc = acc
+        self.ma = ma
         self.id = fighter.fighterCount
         fighter.fighterCount += 1
+
+    # used to determine the winner and loser (which lets be for real, YOU are the loser hahahaha)
     def place(self):
-        if self.hp > 0:
-            win.name = self.name
+        if self.hp != 0:
+            typeWriter(f"{self.name} won!")
         else:
-            lose.name = self.name
+            lossNum += 1
+            if lossNum == 2:
+                extraLoss = " as well"
+            typeWriter(f"{self.name} lost" + extraLoss + "!")
+    
 
 """
 def moves (currently represents attacks, but will at some point affect health and defence and other stats)
 the max and min represent the range of damage
-"""
+
 class move:
     def __init__(self,name,max,min):
         self.name = name
         self.max = max
         self.min = min
-
-# used to determine the winner and loser (which lets be for real, YOU are the loser hahahaha)
-class placement:
-    def __init__(self, name):
-        self.name = name
-win = placement("winner")
-lose = placement("loser")
-
 """
-used to define both the player and enemy
 
-"""
-class character(fighter):
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+
+
 
 # list of different possible moves
 moveList = [
@@ -90,9 +91,14 @@ while moveCount != len(moveList):
 move system of the game, the pokemon part pretty much. 
 Includes an error check to see make sure the user's input is an integer and within the number of choices
 Attacking (move 1):
-- first takes into account the dodge chance and sets variable of dodge to either true or false based on rng. If rng is less than or equal to dodge, dodge will be true and vice versa
-- since the integer value of true is 1 and false is 0. The damage will get mutliplied by 0 when the dodge is True and mutliplied by 1 when dodge is false
+- first takes into account the dodge chance and sets variable of dodge to either true or false based on rng. 
+  If rng is less than or equal to dodge, dodge will be true and vice versa
+- since the integer value of true is 1 and false is 0. The damage will get mutliplied by 0 when the dodge is True 
+  and mutliplied by 1 when dodge is false
 - The target will take damage, or succesfuly dodge where the respective message will be displayed
+
+Healing (move 2):
+- Simple heal that heals by percentage based on rng
 """
 
 def move(choice, attacker, target):
@@ -104,33 +110,54 @@ def move(choice, attacker, target):
         global Repeat
         Repeat = False
         if int_Choice == 1:
-            dodge = target.dg >= r.randint(1,100)
-            dmg = attacker.at * abs(int(dodge)-1)
+            noDodged = target.dg < r.randint(1,100) # equals 0 if target dodged, and 1 if target didn't dodge
+            noMissed = attacker.acc >= r.randint(1,100) # equals 0 if attacker missed, and 1 if attacker didn't miss
+
+            dmg = attacker.at * int(noDodged) * int(noMissed) 
             target.hp -= dmg
-            dodgeTrue = f"{target.name} dodged {attacker.name}'s attack!"
-            dodgeFalse = f"{attacker.name} dealt {dmg} damage to {target.name}"
-            return dodgeTrue if dodge else dodgeFalse
+
+            finalMessageList = [
+            f"{attacker.name} dealt {dmg} damage to {target.name}",
+            f"{target.name} dodged {attacker.name}'s attack!",
+            f"{attacker.name} missed!",
+            f"{target.name} dodged, but {attacker.name} still hit them and dealt {dmg} damage!"
+            ]
+
+            return finalMessageList[int(not noDodged) + 2 * int(not noMissed)]  
         elif int_Choice == 2:
             heal_pcent = r.randrange(10,50, 5)
-            plus_heal = attacker.oghp * heal_pcent / 100
+            plus_heal = attacker.oghp * heal_pcent / 100 * attacker.ma / 100
             attacker.hp += plus_heal
-            return f"{attacker.name} healed {heal_pcent}% or {plus_heal} hp!"
+            finalMessageList[
+            f"{attacker.name} healed {heal_pcent}% or {plus_heal} hp!"
+            ]
+            return finalMessageList[0]
         elif int_Choice > len(moveList):
             print('Make sure to pick a value between 1 and', len(moveList))
             Repeat = True
         else:
             pass
 
+"""
+used to define both the player and enemy
+
+"""
+class character(fighter):
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
 
 # setting up the player and enemy classes
 player = character("", 0)
 enemy = character("", 1)
 
-# list of all the fighterTypes with their stats
+# list of all the fighterTypes with their stats (name, hp, arm, at, dg, acc, ma)
 fighterTypes = [
-fighter("Knight", 200, 30, 5),
-fighter("Barbarian", 100, 50, 10),
-fighter("Archer", 75, 40, 20),
+fighter("Knight", 170, 50, 30, 5, 95, 70),
+fighter("Barbarian", 110, 20, 60, 10, 80, 110),
+fighter("Archer", 75, 25, 50, 20, 60, 125),
+fighter("Wizard", 70, 10, 60, 15, 50, 140),
+fighter("God", 999, 999, 999, 100, 100, 999)
 ]
 
 # exposition
@@ -154,7 +181,7 @@ typeWriter("Select your class by typing either 1,2,etc.")
 
 # sets player and enemy to the same class initially, however repeats the process of selecting a class until both player and enemy have diff classes, possibly change depending on how I feel lolz
 player = enemy = fighterTypes[int(input())-1]
-while enemy.type == player.type:
+while enemy.type == player.type or enemy.type == "God":
     enemy = fighterTypes[r.randint(0,len(fighterTypes)-1)]
 
 # setting up the name of the player again... along with the enemy as well! Will add list of possible enemy names... maybe enemy names based on class idk
@@ -168,24 +195,33 @@ typeWriter(f"Their class will also be {enemy.type}")
 typeWriter(f"why don't we start the battle?")
 
 # the game itself pretty much, everything before was just setup lolz
-while player.hp and enemy.hp > 0:
+while player.hp > 0 and enemy.hp > 0:
     typeWriter(  f"""
-{player.name}'s Health: {player.hp}/{player.oghp}
-{enemy.name}'s Health: {enemy.hp}/{enemy.oghp}
+{player.name} 
+Armor: {player.arm}/{player.ogarm} Health: {player.hp}/{player.oghp} 
+
+{enemy.name} 
+Armor: {enemy.arm}/{enemy.ogarm} Health: {enemy.hp}/{enemy.oghp} 
           """)
     typeWriter(moveStr)
     while Repeat:
-        Pchoice = input()
+        Pchoice = input("Your move: ")
         Pmove = move(Pchoice, player, enemy)
     typeWriter("\n" + Pmove)
-    typeWriter(move(r.randint(1,1), enemy, player) + "\n")
+    Emove = move(r.randint(1,1), enemy, player)
+    typeWriter(Emove + "\n")
     Repeat = True
+    
+    player.hp = 0
+    enemy.hp = 0
+    # player.hp = max(0, player.hp)
+    # enemy.hp = max(0, enemy.hp)
 
 # we're in the endgame now, time to see by how much you lost!
 player.place()
 enemy.place()
-typeWriter(f"{win.name} won!")
-typeWriter(f"{lose.name} lost!")
+
+
 
 
 
